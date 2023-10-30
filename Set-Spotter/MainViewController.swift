da50d0dcef8e46a8b355937f7569d8b598b6ebdd
jdalamo <alamojad@gmail.com>
2023-10-30 15:30:57 -0700

    update welcome page and rename Set-Finder to Set-Spotter

---
+++ Set-Spotter/MainViewController.swift
@@ -0,0 +1,188 @@
+//
+//  ViewController.swift
+//  Set-Spotter
+//
+//  Created by JD del Alamo on 7/13/23.
+//
+
+import UIKit
+import AVFoundation
+
+let FRAME_PROCESSOR_MAX_THREADS = 4 // More than 4 seems to have diminishing returns
+let NUM_SETS_LABEL_DEFAULT_TEXT = " # Sets: " // Leading space is a hacky way to add padding
+
+class MainViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, FrameProcessorDelegate {
+   @IBOutlet weak var imageView: UIImageView!
+   @IBOutlet weak var pauseButton: UIButton!
+   @IBOutlet weak var shareButton: UIButton!
+   @IBOutlet weak var settingsButton: UIButton!
+   @IBOutlet weak var numSetsLabel: UILabel!
+   
+   private let captureSession: AVCaptureSession = AVCaptureSession()
+   private let videoDataOutput = AVCaptureVideoDataOutput()
+   private let frameProcessor = FrameProcessorWrapper(Int32(FRAME_PROCESSOR_MAX_THREADS),
+      showSets: UserDefaults.standard.bool(forKey: HIGHLIGHT_SETS_KEY))
+   private var capturing = true
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+
+      pauseButton.addTarget(self, action: #selector(pauseButtonTapped), for: .touchUpInside)
+      shareButton.addTarget(self, action: #selector(shareButtonTapped), for: .touchUpInside)
+      settingsButton.addTarget(self, action: #selector(settingsButtonTapped), for: .touchUpInside)
+
+      self.addCameraInput()
+      self.getFrames()
+
+      // TODO: update to use UIWindowScene.requestGeometryUpdate()
+      let orientation = getUiOrientation()
+      UIDevice.current.setValue(orientation.rawValue, forKey: "orientation")
+      updateConnectionVideoOrientation(orientation: orientation)
+
+      DispatchQueue.global(qos: .userInitiated).async {
+         self.captureSession.startRunning() // Should be called from background thread
+      }
+   }
+
+   override var supportedInterfaceOrientations: UIInterfaceOrientationMask
+   {
+      return .landscape
+   }
+
+   override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator)
+   {
+      super.viewWillTransition(to: size, with: coordinator)
+      updateConnectionVideoOrientation(orientation: getUiOrientation())
+   }
+
+   @objc private func pauseButtonTapped(sender: UIButton)
+   {
+      self.capturing = !self.capturing
+
+      if (self.capturing) {
+         let image = UIImage(systemName: "pause.circle.fill")?.withRenderingMode(.alwaysTemplate)
+         sender.setImage(image, for: .normal)
+         sender.tintColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+      } else {
+         let image = UIImage(systemName: "play.circle.fill")?.withRenderingMode(.alwaysTemplate)
+         sender.setImage(image, for: .normal)
+         sender.tintColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
+      }
+   }
+
+   @objc private func shareButtonTapped(sender: UIButton)
+   {
+      if (self.capturing) {
+         // User hasn't paused live frame processing--return
+         return
+      }
+
+      let image = self.imageView.image
+      let items = [ image! ]
+      let activityViewController = UIActivityViewController(activityItems: items, applicationActivities: nil)
+      activityViewController.popoverPresentationController?.sourceView = self.view
+
+      self.present(activityViewController, animated: true, completion: nil)
+   }
+
+   @objc private func settingsButtonTapped(sender: UIButton)
+   {
+      if let settingsViewController = storyboard?.instantiateViewController(withIdentifier: "settings")
+         as? SettingsViewController {
+         settingsViewController.delegate = self
+         present(settingsViewController, animated: true, completion: nil)
+      }
+   }
+
+   private func updateConnectionVideoOrientation(orientation: UIInterfaceOrientation)
+   {
+      var videoOrientation: AVCaptureVideoOrientation
+      switch (orientation) {
+         case .landscapeLeft:
+            videoOrientation = .landscapeLeft
+            break
+         case .landscapeRight:
+            videoOrientation = .landscapeRight
+            break
+         default:
+            print("Unsupported device orientation")
+            return
+         }
+
+         guard let connection = self.videoDataOutput.connection(with: AVMediaType.video),
+            connection.isVideoOrientationSupported else {
+            return
+         }
+
+         connection.videoOrientation = videoOrientation
+   }
+
+   private func addCameraInput() {
+      guard let device = AVCaptureDevice.DiscoverySession(
+         deviceTypes: [.builtInWideAngleCamera, .builtInDualCamera, .builtInTrueDepthCamera],
+         mediaType: .video,
+         position: .back
+      ).devices.first else {
+         fatalError("No back camera detected.")
+      }
+
+      let cameraInput = try! AVCaptureDeviceInput(device: device)
+      self.captureSession.addInput(cameraInput)
+   }
+
+   private func getFrames() {
+      videoDataOutput.videoSettings = [
+         (kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String: Any]
+      videoDataOutput.alwaysDiscardsLateVideoFrames = true
+      videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera.frame.processing.queue"))
+      self.captureSession.addOutput(videoDataOutput)
+   }
+
+   func captureOutput(
+      _ output: AVCaptureOutput,
+      didOutput sampleBuffer: CMSampleBuffer,
+      from connection: AVCaptureConnection)
+   {
+      if (!capturing) {
+         return
+      }
+      guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+      CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags.readOnly)
+      let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer)
+      let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+      let width = CVPixelBufferGetWidth(imageBuffer)
+      let height = CVPixelBufferGetHeight(imageBuffer)
+      let colorSpace = CGColorSpaceCreateDeviceRGB()
+      var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Little.rawValue
+      bitmapInfo |= CGImageAlphaInfo.premultipliedFirst.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
+      let context = CGContext(data: baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
+      guard let quartzImage = context?.makeImage() else { return }
+      CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags.readOnly)
+      let image = UIImage(cgImage: quartzImage)
+
+      guard let processedImage = frameProcessor?.process(image) else {
+         fatalError("Problem unwrapping frameProcessor")
+      }
+
+      let numSets = Int((frameProcessor?.getNumSetsInFrame())!)
+
+      DispatchQueue.main.async {
+         self.imageView.image = processedImage
+         self.updateNumSets(numSets: numSets)
+      }
+   }
+
+   private func updateNumSets(numSets: Int)
+   {
+      self.numSetsLabel.text = NUM_SETS_LABEL_DEFAULT_TEXT + String(numSets)
+   }
+
+   func getShowSets() -> Bool {
+      return (frameProcessor?.getShowSets())!
+   }
+   
+   func setShowSets(show: Bool) {
+      frameProcessor?.setShowSets(show)
+   }
+}
+
---
+++ Set-Spotter/cpp/include/FrameProcessor.h
@@ -0,0 +1,129 @@
+//
+//  FrameProcessor.hpp
+//  Set-Spotter
+//
+//  Created by JD del Alamo on 7/19/23.
+//
+
+#pragma once
+
+#include "SetGame.h"
+#include "ThreadPool.h"
+
+#include <opencv2/opencv.hpp>
+
+#include <pthread.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace tp = ThreadPool;
+
+typedef std::vector<cv::Point> Contour;
+typedef std::tuple<int, Contour> IndexedContour;
+
+class ClassifyShapeArg : public tp::PoolTaskArg<std::vector<IndexedContour>> {
+public:
+   ClassifyShapeArg(
+      const std::vector<cv::Vec4i>& _hierarchy,
+      cv::Mat& _frame,
+      std::unordered_map<int, std::vector<SetGame::Shape>>& _cardIndexToShapesMap,
+      pthread_mutex_t* _mapMutex) :
+         hierarchy(_hierarchy),
+         frame(_frame),
+         cardIndexToShapesMap(_cardIndexToShapesMap),
+         mapMutex(_mapMutex) {}
+
+   ClassifyShapeArg() = delete;
+
+   const std::vector<cv::Vec4i>& hierarchy; // Read-only
+   cv::Mat& frame; // Read-only
+   std::unordered_map<int, std::vector<SetGame::Shape>>&
+      cardIndexToShapesMap; // Write
+   pthread_mutex_t* mapMutex;
+};
+
+class FrameProcessor {
+public:
+   FrameProcessor(
+      int maxThreads, bool showSets = true) :
+   _threadPool(maxThreads),
+   _showSets(showSets) {}
+
+   void Process(cv::Mat& frame);
+
+   bool GetShowSets() const { return _showSets; }
+
+   void SetShowSets(bool show) { _showSets = show; }
+
+   int GetNumSetsInFrame() const { return _numSetsInFrame; }
+
+private:
+   /**
+    * ================
+    * Instance Methods
+    * ================
+    */
+   bool cardFilter(
+      const IndexedContour& indexedContour,
+      const std::vector<Contour>& contours,
+      const std::vector<cv::Vec4i>& hierarchy,
+      std::unordered_set<int>& cardIndices) const;
+
+   bool shapeFilter(
+      const IndexedContour& indexedContour,
+      const std::vector<cv::Vec4i>& hierarchy,
+      const std::unordered_set<int>& cardIndices) const;
+
+   std::vector<SetGame::Set> getSortedSets(
+      const std::vector<SetGame::Card> indexedCards) const;
+
+   void highlightSets(
+      cv::Mat& frame,
+      const std::vector<SetGame::Set>& sets,
+      const std::vector<Contour>& contours) const;
+
+   /**
+    * ==============
+    * Static Methods
+    * ==============
+    */
+   static void classifyShapes(
+      void* voidArg);
+
+   static void classifyShape(
+      const IndexedContour& indexedShape,
+      const std::vector<cv::Vec4i>& hierarchy,
+      const cv::Mat& frame,
+      std::unordered_map<int, std::vector<SetGame::Shape>>& cardIndexToShapeMap,
+      pthread_mutex_t* mapMutex);
+
+   static void scaleContour(
+      Contour& contour,
+      const float scalar);
+
+   static cv::Point scalePoint(
+      const cv::Point& point,
+      const int cx,
+      const int cy,
+      const float scalar);
+
+   static double colorDifference(
+      const cv::Scalar& color1,
+      const cv::Scalar& color2);
+
+   static std::tuple<int, int, int> bgrToHsv(
+      const int b,
+      const int g,
+      const int r);
+
+private:
+   bool _initialized = false;
+   tp::ThreadPool _threadPool;
+   float _minCardArea = 0;
+   float _maxCardArea = 0;
+   float _minShapeArea = 0;
+   float _maxShapeArea = 0;
+   int _numSetsInFrame;
+   bool _showSets = true;
+};
